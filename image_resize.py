bf2f4ea4fd7a48818a043de7c8406493d2b1be4c
unknown <chumongkhon.v@gmail.com>
2023-01-15 02:13:41 -0500

    Add an option to keep original width-height ratio of the image, or to use the fixed new_size

--- image_resize.py
+++ image_resize.py
@@ -2,14 +2,35 @@ import os
 from PIL import Image
 
 
-def resize_image(image_path, width, height, save_folder):
+def resize_image(image_path, save_folder, new_size, fixed_size=False,):
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
     
     original_image = Image.open(image_path)
     org_width, org_height = original_image.size
-    print("Successfully resized file: {} from {}x{} to {}x{} pixels".format(image_path, org_width, org_height, width, height))
-    resized_image = original_image.resize((width, height))
+
+    if fixed_size == True:
+        resized_image = original_image.resize((new_size[0], new_size[1]), resample=Image.LANCZOS)
+        print("Successfully resized file: {} from {}x{} to {}x{} pixels".format(image_path, org_width, org_height, new_size[0], new_size[1]))
+
+    else:
+        # check for the original width-height ratio of the image
+        original_ratio = org_width / org_height
+        # Determine the new aspect ratio of the image
+        new_ratio = new_size[0] / new_size[1]
+        if original_ratio > new_ratio:
+            # Case: Width is the limiting factor
+            new_width = new_size[0]
+            new_height = int(new_width / original_ratio)
+            
+        else:
+            # Case: Height is the limiting factor
+            new_height = new_size[1]
+            new_width = int(new_height * original_ratio)
+        resized_image = original_image.resize((new_width, new_height), resample=Image.LANCZOS)
+        print("Successfully resized file: {} from {}x{} to {}x{} pixels".format(image_path, org_width, org_height, new_width, new_height))
+
+    
     # Extract file name
     file_name = os.path.basename(image_path)
     # Build a new file path
--- main.py
+++ main.py
@@ -6,13 +6,16 @@ from excel_generator import generate_excel
 path_to_orig_images_jpg = ".\\original_images_folder"
 path_to_resized_images_folder = '.\\resized_images_folder'
 path_to_excel_output = '.\\excel_output'
-excel_output_file_name = 'output_file.xlsx'
+excel_output_file_name = '.\\output_file.xlsx'
+new_size = (400, 300)
 
 folders = []
+n=1
 for folder in os.listdir(path_to_orig_images_jpg):
     if os.path.isdir(os.path.join(path_to_orig_images_jpg, folder)):
-        print("folder 1: {} \n".format(folder))
+        print("folder {}: {} \n".format(n,folder))
         folders.append(folder)
+        n+=1
 
 print("folders")
 
@@ -24,7 +27,11 @@ for i in folders:
 
     for j in jpg_files:
         try:
-            resize_image(os.path.join(path_to_orig_images_jpg ,i,j), 400, 300, os.path.join(path_to_resized_images_folder,i))
+            resize_image(image_path =os.path.join(path_to_orig_images_jpg ,i,j)
+            , save_folder = os.path.join(path_to_resized_images_folder,i)
+            , new_size = new_size
+            , fixed_size=False    # Change to True is you don't want to keep original ratio
+            )
         except Exception as e:
             print(f"An error occurred while converting the image: {e}")
 
@@ -37,4 +44,4 @@ for i in folders:
     jpg_files = [file for file in file_list if file.endswith(".jpg")]
     images_data[i] = [os.path.join(path_to_resized_images_folder,i ,j) for j in jpg_files]
 print(images_data)
-generate_excel(os.path.join(path_to_excel_output,excel_output_file_name), images_data)
+generate_excel(os.path.join(path_to_excel_output,excel_output_file_name), images_data , new_cell_size =new_size)
